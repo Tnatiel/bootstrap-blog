8f05805ebf91fc509db8fb632385c2304f6a64c8
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-03-04 00:51:49 +0200

    Changed the initial photos. Created endpoint with fake blogs and rendering them instead of the initial posts, reformatted the code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import requests as req
 
+POSTS = req.get("https://api.npoint.io/511981ad9e97d831283f").json()
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=POSTS)
 
 
 @app.route("/about")
@@ -16,6 +18,7 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
