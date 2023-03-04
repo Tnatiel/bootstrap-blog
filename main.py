1662ac19aced44e54b8f2ba1ced6aa78582b3abd
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-03-04 06:09:30 +0200

    Fixed copy right by adding app config processor so all htmls share the year variable

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 import requests as req
 
 POSTS = req.get("https://api.npoint.io/511981ad9e97d831283f").json()
 app = Flask(__name__)
+app.config['CURRENT_YEAR'] = datetime.now().year
+
+
+@app.context_processor
+def year_injector():
+    return {"year": app.config["CURRENT_YEAR"]}
 
 
 @app.route("/")
