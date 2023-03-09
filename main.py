4ac444362a475d94b0726c88a9eeda18b46e5f6e
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-03-10 01:11:21 +0200

    Added register and login endpoint, also conditional rendering of navbar buttons dependes on logged in state, added 2 forms in form.py for register anf login, created user table in db.

--- main.py
+++ main.py
@@ -4,29 +4,41 @@ from datetime import datetime
 from email.mime.text import MIMEText
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_ckeditor import CKEditor, CKEditorField
+from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form, StringField, SubmitField, HiddenField
-from wtforms.validators import InputRequired, URL
+from flask_gravatar import Gravatar
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import CreatePostForm, RegisterForm, LoginForm
 
 EMAIL = "ntgk666@gmail.com"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['CURRENT_YEAR'] = datetime.now().year
 Bootstrap(app)
+
 
 # CKEditor
 ckeditor = CKEditor(app)
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 
+# ---------------------------- Flask Login ----------------------------------
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# ---------------------------- DataBase ----------------------------------
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-# CONFIGURE TABLE
+# CONFIGURE TABLES
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -37,15 +49,27 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-# WTForm
-class CreatePostForm(Form):
-    hidden_tag = HiddenField()
-    title = StringField("Blog Post Title", validators=[InputRequired()])
-    subtitle = StringField("Subtitle", validators=[InputRequired()])
-    author = StringField("Your Name", validators=[InputRequired()])
-    img_url = StringField("Blog Image URL", validators=[InputRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[InputRequired()])
-    submit = SubmitField("Submit Post")
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+
+# ---------------------------- Data Injectors ----------------------------------
+app.config['CURRENT_YEAR'] = datetime.now().year
+app.config["LOGGED_IN"] = False
+
+
+@app.context_processor
+def year_injector():
+    return {"year": app.config["CURRENT_YEAR"]}
+
+
+@app.context_processor
+def is_logged():
+    return {"logged_in": app.config["LOGGED_IN"]}
+# --------------------- Util Functions --------------------------
 
 
 def send_msg(msg):
@@ -57,11 +81,6 @@ def send_msg(msg):
         to_send = MIMEText(msg)
         to_send["Subject"] = "Blog reader"
         server.sendmail(EMAIL, [EMAIL], msg=to_send.as_string())
-
-
-@app.context_processor
-def year_injector():
-    return {"year": app.config["CURRENT_YEAR"]}
 
 # --------------------- END POINTS --------------------------
 
@@ -149,19 +168,47 @@ def delete_post(pid):
 
 
 # --------------------- Users CRUD --------------------------
-@app.route('/register')
+@app.route('/register', methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    form = RegisterForm(request.form)
+    if request.method == "POST" and form.validate():
+        hashed_password = generate_password_hash(form.password.data, salt_length=8)
+        user = User(
+            name=form.name.data,
+            email=form.user_email.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        app.config["LOGGED_IN"] = True
+        return redirect(url_for("home"))
+    return render_template("register.html", form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    form = LoginForm(request.form)
+    error = None
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email=request.form.get("user_email")).first()
+        if not user:
+            error = "That email doesn't exist, please try again"
+        elif check_password_hash(user.password, request.form.get("password")):
+            login_user(user)
+            app.config["LOGGED_IN"] = True
+            return redirect(url_for("home"))
+        else:
+            error = "The password doesn't match, please try again"
+    return render_template("login.html", form=form, error=error)
 
 
 @app.route('/logout')
 def logout():
-    return redirect(url_for('get_all_posts'))
+    logout_user()
+    app.config["LOGGED_IN"] = False
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
