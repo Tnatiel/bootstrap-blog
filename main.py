1fa70e350e4c919cb80aeff93d2ba48aa5550dcb
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-03-09 02:22:24 +0200

    Added sqlite db connection and created blog post table and post form

--- main.py
+++ main.py
@@ -4,15 +4,45 @@ from email.mime.text import MIMEText
 from datetime import datetime
 import requests as req
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, SubmitField
+from wtforms.validators import InputRequired, URL
 
 EMAIL = "ntgk666@gmail.com"
 POSTS = req.get("https://api.npoint.io/511981ad9e97d831283f").json()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['CURRENT_YEAR'] = datetime.now().year
+Bootstrap(app)
 
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+# WTForm
+class CreatePostForm(Form):
+    title = StringField("Blog Post Title", validators=[InputRequired()])
+    subtitle = StringField("Subtitle", validators=[InputRequired()])
+    author = StringField("Your Name", validators=[InputRequired()])
+    img_url = StringField("Blog Image URL", validators=[InputRequired(), URL()])
+    body = StringField("Blog Content", validators=[InputRequired()])
+    submit = SubmitField("Submit Post")
 
 def send_msg(msg):
-    print(msg)
     port = 465  # for SSL
     context = ssl.create_default_context()
 
